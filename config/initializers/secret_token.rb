056e450b8bb50f60a89b10450bcd6d56ee03c7f9
Miklos Vajna <vmiklos@frugalware.org>
2011-10-06 00:16:59 +0200

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todomgr::Application.config.secret_token = '529c7ab8591cc0c31ef23b265108a498693b9e749e40f4f8029560f531d57b1a4f6c80d519e32a8d9fba4c1e221da4aa6bdb60454f300d124e69622d75dc0076'
