0d09c7e5e3837d9ee40069f11c8106cd88e3f69f
Miklos Vajna <vmiklos@frugalware.org>
2011-10-22 14:52:06 +0200

    views/todos/show

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,6 +8,22 @@ class TodosController < ApplicationController
   end
 
   def show
+    vmiklos = User.find :first
+    todo1 = Todo.new
+    todo1.deadline = Time.now
+    todo1.todo = "Fix bugs"
+    todo1.done = false
+    todo1.user_id = vmiklos.id
+    todo1.public = true
+
+    todo2 = Todo.new
+    todo2.deadline = Time.now
+    todo2.todo = "Read RSS"
+    todo2.done = false
+    todo2.user_id = vmiklos.id
+    todo2.public = true
+
+    @todos = [todo1, todo2]
   end
 
 end
