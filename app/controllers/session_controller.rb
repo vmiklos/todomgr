dc585e0cf4f7483dcb8c7a8abd471d172d660a63
Miklos Vajna <vmiklos@vmiklos.hu>
2011-11-01 15:00:31 +0100

    impl SessionController

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,18 @@
 class SessionController < ApplicationController
+  def create
+    @current_user = User.authenticate(params[:username], params[:password])
+    if @current_user
+      session[:user] = @current_user.id
+      redirect_to :back
+    else
+      flash[:notice] = 'Invalid user name or password'
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    reset_session
+    flash[:notice] = 'Logged out successfully'
+    redirect_to :back
+  end
 end
