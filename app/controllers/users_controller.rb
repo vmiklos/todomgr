e1800c6a5e01dbad532e9e8b3b4d008334e26c9b
Miklos Vajna <vmiklos@vmiklos.hu>
2011-11-01 15:29:19 +0100

    UsersController::create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,19 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      @current_user = @user
+      session[:user] = @current_user.id
+      flash[:notice] = 'Successfull registration'
+      redirect_to :controller => "todos", :action => "show"
+    else
+      flash[:notice] = 'User name is already in use'
+      render :action => 'new'
+    end
+  end
+
   def show
   end
 
