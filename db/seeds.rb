ca74cffca5f52fc4c182299646b611e834fcecfe
Miklos Vajna <vmiklos@vmiklos.hu>
2011-11-19 13:59:07 +0100

    more db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,23 @@
 User.create(username: 'vmiklos', password: 'foo', email: 'vmiklos@vmiklos.hu')
 User.create(username: 'akarki', password: 'akarmi', email: 'akarki@mail.bme.hu')
 
-u = User.find_by_username 'akarki'
+u = User.find_by_username 'vmiklos'
 
-Todo.create(deadline: Time.now, todo: 'Fix bugs', done: false, user: u, public: true)
-Todo.create(deadline: Time.now, todo: 'Read RSS', done: false, user: u, public: true)
+month = 60*60*24*30
+
+Todo.create(deadline: Time.now-(month*3)-1, todo: 'Download ruby book', done: true, user: u, public: true)
+Todo.create(deadline: Time.now-(month*3), todo: 'Write slides', done: false, user: u, public: true)
+Todo.create(deadline: Time.now-(month*2)-1, todo: 'Upgrade RSS reader', done: true, user: u, public: true)
+Todo.create(deadline: Time.now-(month*2), todo: 'Read index', done: false, user: u, public: true)
+Todo.create(deadline: Time.now-(month*1)-2, todo: 'Ride the bycicle', done: false, user: u, public: true)
+Todo.create(deadline: Time.now-(month*1)-1, todo: 'Fix bugs', done: true, user: u, public: true)
+Todo.create(deadline: Time.now-(month*1), todo: 'Take out the dog', done: false, user: u, public: true)
+
+
+Todo.create(deadline: Time.now+(month*1), todo: 'Shopping', done: false, user: u, public: true)
+Todo.create(deadline: Time.now+(month*1)+1, todo: 'Write specification', done: true, user: u, public: true)
+Todo.create(deadline: Time.now+(month*1)+2, todo: 'Make some phone call', done: false, user: u, public: true)
+Todo.create(deadline: Time.now+(month*2), todo: 'Arrange hotel reservation', done: false, user: u, public: true)
+Todo.create(deadline: Time.now+(month*2)+1, todo: 'Buy cinema tickets', done: true, user: u, public: true)
+Todo.create(deadline: Time.now+(month*3), todo: 'Attend ruby course', done: false, user: u, public: true)
+Todo.create(deadline: Time.now+(month*3)+1, todo: 'Do the wasing up', done: true, user: u, public: true)
