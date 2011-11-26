d8ed108cd285c02da327318085278703ef081569
Miklos Vajna <vmiklos@vmiklos.hu>
2011-11-26 13:41:30 +0100

    user unit tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "username is present" do
+    u = User.new
+    assert !u.save, "missing username accepted"
+  end
+
+  test "username is unique" do
+    u = User.new
+    u.username = "foobar"
+    assert u.save
+
+    v = User.new
+    v.username = "foobar"
+    assert !v.save, "non-unique username accepted"
+  end
+
+  test "username length is within a range" do
+    u = User.new
+    u.username = "a"
+    assert !u.save
+
+    u.username = "asdfasdfasdfasdfasdfa"
+    assert !u.save
+  end
 end
