0593b9fb390ea84dbd2a4e27ac51ba99b208c1c3
Miklos Vajna <vmiklos@vmiklos.hu>
2011-11-26 14:07:04 +0100

    SessionControllerTest

--- test/functional/session_controller_test.rb
+++ test/functional/session_controller_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class SessionControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @request.env["HTTP_REFERER"]='/todos/show'
+  end
+  test "login" do
+    post :create,
+      {:username => users(:me).username, :password => 'titok'}
+    assert_response :redirect
+    assert_not_nil session[:user]
+    assert_equal session[:user], users(:me).id
+  end
+
+  test "failed login" do
+    post :create,
+      {:username => users(:me).username, :password => 'br0ken'}
+    assert_response :redirect
+    assert_nil session[:user]
+  end
+
+  test "logout" do
+    post :destroy, nil, {:user => users(:me).id}
+    assert_response :redirect
+    assert_nil session[:user]
+  end
 end
